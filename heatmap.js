bb95124ea4d8c7d16e136f2d0c3ce2520e8139a4
adhm271 <aaronmaldan@gmail.com>
2020-11-11 14:48:33 -0800

    add filters and nci and phem links

--- heatmap.js
+++ heatmap.js
@@ -1,14 +1,24 @@
 const $heatmap_container = $('#heatmap');
 const heatmap_directory = $heatmap_container.data('heatmapDir');
 const heatmap_file = heatmap_directory + 'main.csv';
+const URL_nci = 'https://cancer.gov/publications/dictionaries/cancer-drug/def/';
+const URL_pchem = 'https://pubchem.ncbi.nlm.nih.gov/compound/';
 
 d3.csv(heatmap_file).then(function(data) {
+    var select_pathway = document.getElementById("pathwaySel");
+    var select_target = document.getElementById("targetSel");
+    var select_compound_name = document.getElementById("compoundSel");
+    // select_pathway.options[select_pathway.options.length] = new Option('Pathway', 'Pathway', True);
+    var pathways = [];
+    var targets = [];
+    var compound_names = [];
+
 
     data.forEach(function(d) {
-        
+
         d = toFloats(d);
 
-        // organize the information about the drug into one property 
+        // organize the information about the drug into one property
         //to make it easier to segregate from the data on the cells
         d.info = {
             'pathway_sort' : d['sort number'],
@@ -16,9 +26,33 @@ d3.csv(heatmap_file).then(function(data) {
             'cmpd_number' :  d.cmpd_number,
             'primary_target' : d['Primary Target'],
             'pathway' : d.Pathway,
+            'URL_nci' : URL_nci + d.cmpd_name,
+            'URL_pchem': URL_pchem + d.cmpd_name,
+            'row_class' : make_class_name(d.cmpd_name)
+
         };
-        
-        // delete the original info from the row so that 
+
+        //make class for each attribute: pathway, cmpnd, and target so you can manipulate here.
+        //make class each
+
+
+
+
+        if (!(pathways.includes(d.Pathway))) {
+            select_pathway.options[select_pathway.options.length] = new Option(d.Pathway, d.Pathway);
+            pathways.push(d.Pathway);
+        }
+
+        if (!(targets.includes(d['Primary Target']))) {
+            select_target.options[select_target.options.length] = new Option(d['Primary Target'], d['Primary Target']);
+            targets.push(d['Primary Target']);
+        }
+        if (!(compound_names.includes(d.cmpd_name))) {
+            select_compound_name.options[select_compound_name.options.length] = new Option(d.cmpd_name, d.cmpd_name);
+            compound_names.push(d.cmpd_name);
+        }
+
+        // delete the original info from the row so that
         //we can control its display separately
         delete d.cmpd_number;
         delete d.cmpd_name;
@@ -26,15 +60,15 @@ d3.csv(heatmap_file).then(function(data) {
         delete d['Primary Target'];
         delete d.Pathway;
     });
-    
+
     var table = d3.select("#heatmap").append("table");
-    
+
     var header = table.append("thead").append("tr");
     var tableHeaders = Object.keys(data[0]);
     var sortTypes = [];
-    
+
     for (var i = 0; i < tableHeaders.length; i++) {
-        // remove the 'info' key from the table headers since 
+        // remove the 'info' key from the table headers since
         // we will add from it what we want to show manually
         if (tableHeaders[i] == 'info') {
             tableHeaders.splice(i, 1);
@@ -42,12 +76,12 @@ d3.csv(heatmap_file).then(function(data) {
             sortTypes.push('float');
         }
     }
-    
+
     // add in table headers do display drug info
     // "yes" for Pathway Sort is for marking it as the default sort column
-    tableHeaders.unshift(["Pathway Sort", "yes"], "Pathway", "Target", "Compound Name");
-    sortTypes.unshift('float', 'string', 'string', 'string');
-    
+    tableHeaders.unshift(["Pathway Sort", "yes"], "Pathway", "Target", "Compound Name", "NCI Link", "PubChem link");
+    sortTypes.unshift('float', 'string', 'string', 'string', '','');
+
     header
         .selectAll("th")
         .data(tableHeaders)
@@ -68,9 +102,9 @@ d3.csv(heatmap_file).then(function(data) {
                 return d;
             }
         });
-        
+
     var tBody = table.append("tbody");
-    
+
     var rows = tBody.selectAll("tr")
         .data(data)
         .enter()
@@ -78,12 +112,12 @@ d3.csv(heatmap_file).then(function(data) {
         .attr("class", function(d) {
             return d.info.row_class;
         })
-        
+
     var color = d3.scaleLinear()
-            .domain([0, 25])
-            .range(["#12bab8","#e63f3b"])
-            .interpolate(d3.interpolateHcl);
-        
+        .domain([0, 25])
+        .range(["#12bab8","#e63f3b"])
+        .interpolate(d3.interpolateHcl);
+
     var cells = rows
         .selectAll("td")
         .data(function (d) {
@@ -100,7 +134,7 @@ d3.csv(heatmap_file).then(function(data) {
         })
         .attr("data-cmpd", function (d) {
             return d.cmpd_name;
-            
+
         })
         .attr("data-cmpd_number", function (d) {
             return  d.cmpd_number;
@@ -108,29 +142,34 @@ d3.csv(heatmap_file).then(function(data) {
         .attr("data-ic50", function (d) {
             if (!isNaN(d.ic50)) {
                 return d.ic50;
-            } 
+            }
         })
         .on("click", function () {
             showGraph(this);
         })
         .attr("class", function (d) {
-            // add a class so that the colored cells can be styled 
+            // add a class so that the colored cells can be styled
             //differently than the rest of the table
             if (!isNaN(d.ic50)) {
                 return "heatmap__cell--" + typeof d.ic50;
-            } 
+            }
         })
         .style("background-color", function (d, i) {
             if (!isNaN(d.ic50)) {
                 return color(d.ic50);
-            } 
+            }
         })
         .append("span")
-        .text(function (d) {
+        .html(function (d) {
+            // console.log(d);
+            if (d.link) {
+                return '<a href="' + d.link + '"">' + d.value + '</a>';
+            }
             return d.value;
-            
+
         });
-        
+
+
     // add data-sort to the table headers and use the stupid table plugin to enable sorting
     d3.select("#heatmap")
         .selectAll("th")
@@ -139,22 +178,194 @@ d3.csv(heatmap_file).then(function(data) {
             return d;
         });
     $("table").stupidtable();
-    
+
     $('#heatmap-search').hideseek();
-    
+
     $('#heatmap .fa-spinner').hide();
 
 
 });
+
+function make_class_name(strng){
+    return strng.replace(/[^a-zA-Z0-9 -]/g, '').replace(/\s+/g, '-').replace(/-+/g, '-'); // collapse dashes
+}
+
+// arguments: reference to select list, callback function (optional)
+function getSelectedOptions(sel, fn) {
+    var opts = [], opt;
+
+    // loop through options in select list
+    for (var i=0, len=sel.options.length; i<len; i++) {
+        opt = sel.options[i];
+
+        // check if selected
+        if ( opt.selected ) {
+            // add to array of option elements to return from this function
+            opts.push(opt);
+
+            // invoke optional callback function if provided
+            if (fn) {
+                fn(opt);
+            }
+        }
+    }
+
+    // return array containing references to selected option elements
+    // console.log(opts)
+    return opts;
+}
+
+
+//for pathways
+// anonymous function onchange for select list with id demoSel
+document.getElementById("pathwaySel").onchange = function(e) {
+    // get reference to display textarea
+    var display = document.getElementById('display');
+    display.innerHTML = ''; // reset
+
+    getSelectedOptions(this, callback);
+
+
+    // remove ', ' at end of string
+    var str = display.innerHTML.slice(0, -2);
+
+    display.innerHTML = str;
+};
+
+document.getElementById('demoForm').onsubmit = function(e) {
+    // reference to select list using this keyword and form elements collection
+    // no callback function used this time
+
+
+    var opts = getSelectedOptions(this.elements['demoSel[]']);
+    if (isset($_POST['clear_button'])) {
+        var opts = []
+    }
+
+
+    alert('The number of options selected is: ' + opts.length);  //  number of selected options
+
+    return false; // don't return online form
+};
+
+function callback(opt) {
+    // display in textarea for this example
+    var display = document.getElementById('display');
+    display.innerHTML += opt.value + ', ';
+
+    // can access properties of opt, such as...
+    //alert( opt.value )
+    //alert( opt.text )
+    //alert( opt.form )
+}
+
+
+//for targets
+// anonymous function onchange for select list with id demoSel
+document.getElementById("targetSel").onchange = function(e) {
+    // get reference to display textarea
+    var display = document.getElementById('display1');
+    display.innerHTML = ''; // reset
+
+    // callback fn handles selected options
+    getSelectedOptions(this, callback1);
+
+    // remove ', ' at end of string
+    var str = display.innerHTML.slice(0, -2);
+    display.innerHTML = str;
+};
+document.getElementById('demoForm1').onsubmit = function(e) {
+    // reference to select list using this keyword and form elements collection
+    // no callback function used this time
+    var opts = getSelectedOptions(this.elements['demoSel1[]']);
+    if (isset($_POST['clear_button'])) {
+        var opts = []
+    }
+
+    alert('The number of options selected is: ' + opts.length); //  number of selected options
+
+    return false; // don't return online form
+};
+// example callback function (selected options passed one by one)
+function callback1(opt) {
+    // display in textarea for this example
+    var display = document.getElementById('display1');
+    display.innerHTML += opt.value + ', ';
+
+    // can access properties of opt, such as...
+    //alert( opt.value )
+    //alert( opt.text )
+    //alert( opt.form )
+}
+
+//for compound names
+// anonymous function onchange for select list with id demoSel
+document.getElementById("compoundSel").onchange = function(e) {
+    // get reference to display textarea
+    var display = document.getElementById('display2');
+    display.innerHTML = ''; // reset
+
+    // callback fn handles selected options
+    var selected_opts = getSelectedOptions(this, callback2);
+
+    var classes = []
+    for (var i = 0;i < selected_opts.length;i++){
+        classes.push(make_class_name(selected_opts[i].innerHTML))
+    }
+    $('#heatmap tbody tr').each(function( index ) {
+        // console.log( index + ": " + $( this ).text() );
+        $(this).hide();
+        for (var i = 0;i < classes.length;i++) {
+            if ($(this).hasClass(classes[i])){
+                $(this).show()
+            }
+                }
+    });
+
+
+
+    // remove ', ' at end of string
+    var str = display.innerHTML.slice(0, -2);
+    display.innerHTML = str;
+};
+
+document.getElementById('demoForm2').onsubmit = function(e) {
+    // reference to select list using this keyword and form elements collection
+    // no callback function used this time
+    var opts = getSelectedOptions(this.elements['demoSel2[]']);
+    if (isset($_POST['clear_button'])) {
+        var opts = []
+    }
+
+    alert('The number of options selected is: ' + opts.length); //  number of selected options
+
+    return false; // don't return online form
+};
+
+// example callback function (selected options passed one by one)
+function callback2(opt) {
+    // display in textarea for this example
+    var display = document.getElementById('display2');
+    display.innerHTML += opt.value + ', ';
+
+    // can access properties of opt, such as...
+    //alert( opt.value )
+    //alert( opt.text )
+    //alert( opt.form )
+}
+
 
 function getRowValues(d) {
     var values = [
         {'value': d.info.pathway_sort, 'sort': null},
         {'value': d.info.pathway, 'sort': null},
         {'value': d.info.primary_target, 'sort': null},
-        {'value': d.info.cmpd_name, 'sort': d.info.cmpd_number}
+        {'value': d.info.cmpd_name, 'sort': d.info.cmpd_number},
+        {'value': 'link', 'link': d.info.URL_nci, 'sort':null},
+        {'value': 'link', 'link': d.info.URL_pchem, 'sort':null},
+
     ];
-        
+
     return values.concat(getObjValuesAsArray_compounds(d));
 }
 
@@ -176,7 +387,7 @@ function getObjValuesAsArray_compounds (o) {
                 'ic50' : o[k]
             }
             arr.push(cell);
-        } 
+        }
     });
     return arr;
 }
@@ -218,21 +429,21 @@ function showGraph (cell) {
 // rounding information: https://github.com/d3/d3-format#precisionRound
 
 function drawGraph (cell_line, compound_number, ic50/*, HillSlope*/) {
-    
+
     const margin = {top: 75, right: 50, bottom: 50, left: 50}
-            , width = 500 - margin.left - margin.right // Use the window's width
-            , height = 550 - margin.top - margin.bottom; // Use the window's height
+        , width = 500 - margin.left - margin.right // Use the window's width
+        , height = 550 - margin.top - margin.bottom; // Use the window's height
 
     const xScale = d3.scaleLinear()
         .domain([-3, 2]) // input
         .range([0, width]); // output
-        
+
     const svg = d3.select("#dose-graph").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-            
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
     console.log(heatmap_directory + cell_line + ".csv");
 
     d3.csv(heatmap_directory + cell_line + ".csv").then(function(original_data) {
@@ -264,22 +475,22 @@ function drawGraph (cell_line, compound_number, ic50/*, HillSlope*/) {
                         dataset.push(point);
                     }
                 }
-                
+
             });
         });
         original_data = undefined;
-        
+
         if (dataset.length < 1) {
-            
+
             svg.append("text")
                 .attr("y", margin.top)
                 .attr("x", margin.left)
                 .attr("dy", "1em")
                 .style("text-anchor", "middle")
                 .text("Data not found.");
-                
+
             return false;
-            
+
         }
 
         let max_inhibitions = d3.max(dataset, function(d) { return d.inhibition;} );
@@ -349,7 +560,7 @@ function drawGraph (cell_line, compound_number, ic50/*, HillSlope*/) {
             .attr("dy", "1em")
             .style("text-anchor", "middle")
             .text("Normalized % Inhibition");
-            
+
         // add graph title
         svg.append("text")
             .attr("x", (width / 2))
@@ -366,7 +577,7 @@ function drawGraph (cell_line, compound_number, ic50/*, HillSlope*/) {
             .style("font-size", "18px")
             .text("IC50 = " + ic50 + " | Hill Slope = " + HillSlope);
 
-        
+
         // Append the path, bind the data, and call the line generator
         if (HillSlope && !isNaN(HillSlope)) {
             svg.append("path")
@@ -384,23 +595,23 @@ function drawGraph (cell_line, compound_number, ic50/*, HillSlope*/) {
             .attr("cy", function(d) { return yScale(d.inhibition) })
             .attr("r", 5)
             .append("title").text(function(d) {
-                    return "dose: " +  fmt(d.dose) + "\n"
-                        + "inhibition: " + fmt(d.inhibition);
-                });
+            return "dose: " +  fmt(d.dose) + "\n"
+                + "inhibition: " + fmt(d.inhibition);
+        });
 
-        
+
 
     })
-    .catch(function(error) {
-        
-        svg.append("text")
-            .attr("y", margin.top)
-            .attr("x", margin.left)
-            .attr("dy", "1em")
-            .style("text-anchor", "middle")
-            .text("Data not found.");
+        .catch(function(error) {
 
-    });
+            svg.append("text")
+                .attr("y", margin.top)
+                .attr("x", margin.left)
+                .attr("dy", "1em")
+                .style("text-anchor", "middle")
+                .text("Data not found.");
+
+        });
 }
 
 function dose_response_curve(concentration, top, bottom, LogIC50, HillSlope) {
